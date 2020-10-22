f1ff32b414e00ae7de4a74ed54bf3715b5b06751
Greg Malone <atx76dragon@yahoo.com>
2020-10-22 16:18:38 -0500

    Created queries in the seeds.sql, for the schema.sql database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheddar Cheeseburger", TRUE), ("Western", TRUE), ("Swiss & Mushrooms", FALSE),
+("Hawaiian", FALSE), ("Hippie Burger", FALSE), ("Classic", FALSE), ("Patty Melt", TRUE), ("Baconator", TRUE), ("Big Mac", FALSE)
